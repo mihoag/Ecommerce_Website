db16681e25a1eee13e390b1d1b8766d6869735fb
21120453 <phuonghieuto@gmail.com>
2024-01-18 20:15:20 +0700

    working

--- routes/sites/admin/admin.r.js
+++ routes/sites/admin/admin.r.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/dashboard', require('./dashboard.r'))
-router.get('/customers', require('./customers.r'))
 router.get('/brands', require('./brands.r'))
+router.get('/customers', require('./customers.r'))
 router.get('/products', require('./products.r'))
 router.get('/orders', require('./orders.r'))
 router.get('/statistics', require('./statistics.r'))
