c9ac3fc03d6f6084e457eb4556a65e8181cc67a1
21120447 <haonhat2729@gmail.com>
2024-01-19 22:50:47 +0700

    add statistics page

--- controllers/sites/admin/statistics.c.js
+++ controllers/sites/admin/statistics.c.js
@@ -1,15 +1,47 @@
+const statisticsM = require('../../../models/statistics.m');
+
 class statisticsController {
-    async showStatistics(req, res, next) {
-        try {
-            res.render("admin/statistics", {
-                layout: "adminLayout",
-                title: "Statistics",
-                isStatistics: true,
-                css: "statistics"
-              });
-        } catch (error) {
-            next(error);
-        }
+  async showStatistics(req, res, next) {
+    try {
+      res.render("admin/statistics", {
+        layout: "adminLayout",
+        title: "Statistics",
+        isStatistics: true,
+        css: "statistics",
+        js: 'statistics'
+      });
+    } catch (error) {
+      next(error);
     }
+  }
+
+  async getDataRevenue(req, res, next) {
+    try {
+      const { month, year } = req.params;
+      const rs = await statisticsM.getRevenue(month, year);
+      res.json(rs);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getDataRevenueW(req, res, next) {
+    try {
+      let { from, to } = req.params
+      const rs = await statisticsM.getRevenueWeek(from, to);
+      res.json(rs);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getTop5Products(req, res, next) {
+    try {
+      const rs = await statisticsM.getTop5();
+      return res.json(rs);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 module.exports = new statisticsController();
--- routes/sites/admin/admin.r.js
+++ routes/sites/admin/admin.r.js
@@ -6,7 +6,7 @@ router.use('/customers', require('./customers.r'))
 router.get('/brands', require('./brands.r'))
 router.get('/products', require('./products.r'))
 router.use('/orders', require('./orders.r'))
-router.get('/statistics', require('./statistics.r'))
+router.use('/statistics', require('./statistics.r'))
 router.get('/settings', require('./settings.r'))
 
 module.exports = router;
