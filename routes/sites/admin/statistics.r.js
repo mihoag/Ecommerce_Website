ac047be09849adf47bb9f98cab2b07846c904b09
21120453 <phuonghieuto@gmail.com>
2024-01-25 01:26:18 +0700

    fix merge conflict

--- routes/sites/admin/statistics.r.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const statisticsController = require('../../../controllers/sites/admin/statistics.c')
-
-router.get('/statistics', statisticsController.showStatistics)
-router.get('/', statisticsController.showStatistics)
-module.exports = router;
